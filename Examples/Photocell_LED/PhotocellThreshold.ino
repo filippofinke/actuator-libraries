994195da4aa6deeeb91ac41480cdae779848b997
filippofinke <filippo.finke@samtrevano.ch>
2018-12-12 14:10:40 +0100

    Fix duplicates

--- Examples/Photocell_LED/PhotocellThreshold.ino
+++ Examples/Photocell_LED/PhotocellThreshold.ino
@@ -2,7 +2,7 @@
  * @Author: filippofinke
  * @Date:   23.11.2018
  * @Last modified by:   filippofinke
- * @Last modified time: 07.12.2018
+ * @Last modified time: 12.12.2018
  */
  /*
  Descrizione:
--- Examples/Photocell_LED/PhotocellToLEDIntensity.ino
+++ Examples/Photocell_LED/PhotocellToLEDIntensity.ino
@@ -2,12 +2,7 @@
  * @Author: filippofinke
  * @Date:   23.11.2018
  * @Last modified by:   filippofinke
- * @Last modified time: 07.12.2018
- */
- /*
- Descrizione:
- Imposta il valore ricavato dalla fotocellula come luminosità del
- led.
+ * @Last modified time: 12.12.2018
  */
 
 /**
@@ -19,28 +14,24 @@
 /**
  * Istanzio un oggetto di tipo Photocell.
  */
-Photocell photocell(4);
-
+Photocell photocell(3);
 /**
  * Istanzio un oggetto di tipo Led.
  */
-Led led(1);
-
+Led led(4);
 /**
  * Imposto un valore minimo di luminosità della resistenza.
  */
 int minLux = 0;
-
 /**
  * Imposto un valore massimo di luminosità della resistenza.
  */
-int maxLux = 400;
+int maxLux = 1023;
 
 /**
  * Metodo di setup, viene eseguito una sola volta.
  */
 void setup() {
-
 }
 
 /**
@@ -50,7 +41,7 @@ void loop() {
   //Ricavo la luminosità.
   int lux = photocell.getLux();
   //Converto il valore di luminosità in un altra scala.
-  int analogValue = map(lux, minLux, maxLux, 0, 1023);
+  int analogValue = map(lux, minLux, maxLux, 0, 255);
   //Imposto la luminosità del led.
   led.setAnalogState(analogValue);
 }
